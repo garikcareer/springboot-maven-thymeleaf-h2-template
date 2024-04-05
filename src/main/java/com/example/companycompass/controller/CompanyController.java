15530b78fa0f16461cd3edabfd9d58d51843288f
Garik <garikcareer@gmail.com>
2024-04-04 22:34:52 -0400

    Added edit function to edit the company, later to fix

--- src/main/java/com/example/companycompass/controller/CompanyController.java
+++ src/main/java/com/example/companycompass/controller/CompanyController.java
@@ -3,6 +3,7 @@ package com.example.companycompass.controller;
 import com.example.companycompass.model.Company;
 import com.example.companycompass.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +30,6 @@ public class CompanyController {
 
     @DeleteMapping("/delete/{companyId}")
     public ModelAndView deleteCompany(@PathVariable Long companyId, RedirectAttributes redirectAttributes, Model model) {
-
         try {
             if (companyService.existsById(companyId)) {
                 companyService.deleteCompanyById(companyId);
@@ -46,6 +46,25 @@ public class CompanyController {
         model.addAttribute("pageTitle", "Companies");
         List<Company> companies = companyService.getCompanies();
         model.addAttribute("companyList", companies);
+        return new ModelAndView("layout");
+    }
+
+    @GetMapping("/edit/{companyId}")
+    public ModelAndView editCompany(@PathVariable Long companyId, RedirectAttributes redirectAttributes, Model model){
+        Company company = new Company();
+        try {
+            if (companyService.existsById(companyId)) {
+                companyService.updateCompany(company, companyId);
+                redirectAttributes.addFlashAttribute("successMessage", "Company was edited successfully");
+            }
+            else {
+                redirectAttributes.addFlashAttribute("errorMessage", "Company does not exist");
+            }
+        }
+        catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Failed to edit company: " + e.getMessage());
+        }
+        model.addAttribute("content", "company");
         return new ModelAndView("layout");
     }
 }
