5d24fbf702bca05ed8ac352f75842911361da7da
Garik <garikcareer@gmail.com>
2024-04-16 22:38:36 -0400

    Refactoring

--- src/main/java/com/example/companycompass/repository/CompanyRepository.java
+++ src/main/java/com/example/companycompass/repository/CompanyRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompanyRepository extends CrudRepository<Company, Long> { }
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+}
